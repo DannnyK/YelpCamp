fd0aec9b9fabdd88e85ce582500829225a66d879
danielKruger101 <113841516+DannnyK@users.noreply.github.com>
2022-10-13 15:48:29 +0200

    ++

--- routes/users.js
+++ routes/users.js
@@ -11,14 +11,16 @@ router.get("/register", async (req, res) => {
 
 router.post(
 	"/register",
-	catchAsync(async (req, res) => {
+	catchAsync(async (req, res, next) => {
 		try {
 			const { email, username, password } = req.body;
 			const user = new User({ email, username });
 			const registeredUser = await User.register(user, password);
-			console.log(registeredUser);
-			req.flash("success", "Welcome to YelpCamp baby");
-			res.redirect("/campgrounds");
+			req.login(registeredUser, (err) => {
+				if (err) return next(err);
+				req.flash("success", "Welcome to YelpCamp baby");
+				res.redirect("/campgrounds");
+			});
 		} catch (e) {
 			req.flash("error", e.message);
 			res.redirect("register");
@@ -37,12 +39,30 @@ router.post(
 	passport.authenticate("local", {
 		failureFlash: true,
 		failureRedirect: "/login",
+		keepSessionInfo: true,
 	}),
 	(req, res) => {
-		req.flash("success", "Welcome");
-		res.redirect("/campgrounds");
+		req.flash("success", "welcome back!");
+		const redirectUrl = req.session.returnTo || "/campgrounds";
+		delete req.session.returnTo;
+		res.redirect(redirectUrl);
 	}
 );
+//old code
+// router.post(
+// 	"/login",
+// 	passport.authenticate("local", {
+// 		failureFlash: true,
+// 		failureRedirect: "/login",
+// 		keepSessionInfo: true,
+// 	}),
+// 	(req, res) => {
+// 		req.flash("success", "Welcome");
+// 		const redirectUrl = req.session.returnTo || "/campgrounds";
+// 		res.redirect(redirectUrl);
+// 	}
+// );
+
 //end login
 
 //logout
