6a38f11a1a252deb33e678597d7b74a92a4295df
danielKruger101 <113841516+DannnyK@users.noreply.github.com>
2022-10-13 15:13:06 +0200

    added authentication haha

---
+++ routes/users.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user");
+const catchAsync = require("../utils/catchAsync");
+const passport = require("passport");
+
+//register
+router.get("/register", async (req, res) => {
+	res.render("./users/register");
+});
+
+router.post(
+	"/register",
+	catchAsync(async (req, res) => {
+		try {
+			const { email, username, password } = req.body;
+			const user = new User({ email, username });
+			const registeredUser = await User.register(user, password);
+			console.log(registeredUser);
+			req.flash("success", "Welcome to YelpCamp baby");
+			res.redirect("/campgrounds");
+		} catch (e) {
+			req.flash("error", e.message);
+			res.redirect("register");
+		}
+	})
+);
+//end register
+
+//login
+router.get("/login", (req, res) => {
+	res.render("users/login");
+});
+
+router.post(
+	"/login",
+	passport.authenticate("local", {
+		failureFlash: true,
+		failureRedirect: "/login",
+	}),
+	(req, res) => {
+		req.flash("success", "Welcome");
+		res.redirect("/campgrounds");
+	}
+);
+//end login
+
+//logout
+router.get("/logout", (req, res) => {
+	req.logout(function (err) {
+		if (err) {
+			return next(err);
+		}
+	});
+	req.flash("success", "successfully logged out");
+	return res.redirect("/campgrounds");
+});
+
+module.exports = router;
