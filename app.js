f60e8deb3a84d11198e76174ba9ed2818ada9554
danielKruger101 <danielkruger98@gmail.com>
2022-10-17 12:15:27 +0200

    connected to online database

--- app.js
+++ app.js
@@ -13,12 +13,13 @@ const methodOverride = require("method-override");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user");
+const dbUrl = process.env.DB_URL
 
 const userRoutes = require("./routes/users");
 const campgroundsRoutes = require("./routes/campgrounds");
 const reviewsRoutes = require("./routes/reviews");
-
-mongoose.connect("mongodb://localhost:27017/yelp-camp", {
+// 
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
